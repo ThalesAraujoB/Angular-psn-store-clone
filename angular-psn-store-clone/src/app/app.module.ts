e518261c6cc8a8e98a8cc375dd625726731c9ae4
Thales Santos <105941637+ThalesAraujoB@users.noreply.github.com>
2023-05-18 21:42:36 +0000

    finalização-projeto

--- angular-psn-store-clone/src/app/app.module.ts
+++ angular-psn-store-clone/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { CardLabelComponent } from './components/card/card-label/card-label.component';
+import { CardPricingComponent } from './components/card/card-pricing/card-pricing.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { PagesComponent } from './app/pages/pages.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    CardLabelComponent,
+    CardPricingComponent,
+    MenuBarComponent,
+    PagesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
